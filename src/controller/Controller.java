17fc9204052a453b632fdb7d3a302fbdddee7436
JakobW <j.wilmesmeier@gmail.com>
2020-10-23 10:24:26 +0200

    Datenschnittstelle im Controller implementiert

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,8 +1,15 @@
 package controller;
 
+import model.CurrentData;
 import view.MainWindow;
 
 public class Controller {
+	private CurrentData data;
+	
+	public Controller() {
+		this.data = new CurrentData();
+	}
+	
 	public static void main(String[] args) {
 		MainWindow home = new MainWindow();
 		home.init();
