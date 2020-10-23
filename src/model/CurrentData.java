6c4f557898b90d44538e7b5dee3fe180be33dbe9
JakobW <j.wilmesmeier@gmail.com>
2020-10-23 10:20:26 +0200

    Zugriffsschnittstelle für das Model eingefügt

---
+++ src/model/CurrentData.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Data-Access for the model
+ */
+@SuppressWarnings("deprecation")
+public class CurrentData extends AbstractModel {
+	private ArrayList<Reviewer> reviewer;
+
+	public CurrentData() {
+		this.reviewer = new ArrayList<Reviewer>();
+	}
+	
+	public void addReviewer(Reviewer reviewer) {
+		this.reviewer.add(reviewer);
+		this.notifyObservers();
+		this.setChanged();
+	}
+
+	public ArrayList<Reviewer> getReviewer() {
+		return reviewer;
+	}
+
+	public void setReviewer(ArrayList<Reviewer> reviewer) {
+		this.reviewer = reviewer;
+		this.notifyObservers();
+		this.setChanged();
+	}
+}
