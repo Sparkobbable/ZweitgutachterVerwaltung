25ab6203f92a6c1d53321644cbcee5328e69082d
jpfrehe <Jp.Frehe@googlemail.com>
2020-11-08 17:48:27 +0100

    Implementierung NavigationStack

--- src/controller/statecontrollers/ApplicationStateController.java
+++ src/controller/statecontrollers/ApplicationStateController.java
@@ -2,6 +2,7 @@ package controller.statecontrollers;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Stack;
 import java.util.logging.Logger;
 
 import controller.Controller;
@@ -25,9 +26,15 @@ public class ApplicationStateController {
 	 */
 	private Map<ApplicationState, AbstractStateController> stateControllers;
 
+	/**
+	 * Navigation stack which stores the visited ApplicationStates
+	 */
+	private Stack<ApplicationState> visitedStates;
+	
 	public ApplicationStateController(Model model, View view) {
 		this.view = view;
 		this.model = model;
+		visitedStates = new Stack<>();
 		stateControllers = new HashMap<>();
 		stateControllers.put(ApplicationState.HOME, new HomeStateController(view, this, model));
 		stateControllers.put(ApplicationState.REVIEWER_OVERVIEW,
@@ -36,9 +43,8 @@ public class ApplicationStateController {
 		stateControllers.put(ApplicationState.STATE_CHOOSER, new StateChooserStateController(view, this, model));
 		stateControllers.put(ApplicationState.THESIS_ASSIGNMENT, new ThesisAssignmentStateController(view, this, model));
 
-		//TODO add nav stack
 		this.view.atAnyState().addEventHandler(EventId.BACK,
-				(params) -> this.switchState(ApplicationState.HOME));
+				(params) -> this.switchState(this.getLastVisitedState()));
 	}
 
 	/**
@@ -51,5 +57,27 @@ public class ApplicationStateController {
 				.info(String.format("Switched to ApplicationState: %s", applicationState));
 		this.model.setApplicationState(applicationState);
 		view.switchState(applicationState); // TODO remove, add observer
+		
+		if(this.visitedStates.empty() || !this.visitedStates.peek().equals(applicationState)) {
+			this.visitedStates.push(applicationState);
+		}
+	}
+	
+	/**
+	 * Sets the first ApplicationState in the navigation stack to the one before and returns it
+	 * 
+	 * @return ApplicationState Last visited ApplicatonState
+	 */
+	private ApplicationState getLastVisitedState() {
+		if(this.visitedStates.empty()) {
+			return ApplicationState.HOME;
+		} else {
+			this.visitedStates.pop().toString();
+			if(this.visitedStates.empty()) {
+				return ApplicationState.HOME;
+			} else {
+				return this.visitedStates.peek();
+			}
+		}
 	}
 }
