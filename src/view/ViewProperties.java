465b365affcaf1551c6cf0cdb19d978141c2dd0f
Jakob <j.wilmesmeier@gmail.com>
2020-12-07 15:57:33 +0100

    Anpassung Fenstergröße

--- src/view/ViewProperties.java
+++ src/view/ViewProperties.java
@@ -12,7 +12,7 @@ public interface ViewProperties {
 	public static final Color BACKGROUND_COLOR = new Color(36, 50, 63);
 	public static final float BUTTON_FONT_SIZE = 16f;
 	public static final float MENU_BUTTON_FONT_SIZE = 12f;
-	public static final int DEFAULT_HEIGHT = 800;
+	public static final int DEFAULT_HEIGHT = 950;
 	public static final int DEFAULT_WIDTH = 1200;
 	public static final long NOTIFICATION_TIMER = 4;
 	
