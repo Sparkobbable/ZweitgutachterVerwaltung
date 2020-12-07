e15b9705c516c0a643c859bf0984fd62f601a946
Jan Philip <Frehe>
2020-12-07 22:07:03 +0100

    finale Todos

--- src/view/tableModels/CollaboratingReviewerTableModel.java
+++ src/view/tableModels/CollaboratingReviewerTableModel.java
@@ -14,7 +14,7 @@ public class CollaboratingReviewerTableModel extends AbstractDataTableModel<Revi
 	private static final Column<Reviewer, String> NAME = Column.of("Name", Reviewer::getName, String.class);
 //	private final Column<Reviewer, Integer> COLLABORATION_COUNT = Column.of("Anzahl gemeinsamer Bachelorarbeiten",
 //			r -> r.getReviewsWithselectedReviewerCount(this.model.getSelectedReviewer()), Integer.class);
-	
+	//TODO find a solution for the problem, that the column needs to be static, but can not access the model being static
 	public CollaboratingReviewerTableModel(List<Column<Reviewer, ?>> columns, List<Predicate<Reviewer>> filters) {
 		super(List.of(NAME), filters);
 	}
