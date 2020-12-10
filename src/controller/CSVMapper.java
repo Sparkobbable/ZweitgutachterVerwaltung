466cb96b6652e0e152466f6a00731bcd74b0b7af
Leo <Leo@LAPTOP-QDFL0UOH.fritz.box>
2020-12-10 16:35:47 +0100

    erste export Ideen

--- src/controller/CSVMapper.java
+++ src/controller/CSVMapper.java
@@ -1,11 +1,13 @@
 package controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
 import model.Pair;
@@ -28,19 +30,16 @@ public class CSVMapper {
 		return Pair.of(listreviewer, bachelorThesis);
 	}
 
-	private static Pair<List<Reviewer>, List<BachelorThesis>> createObjekts(CSVRecord csvRecord, List<BachelorThesis> bachelorThesis,
-			List<Reviewer> listreviewer, List<Author> listauthor) {
+	private static Pair<List<Reviewer>, List<BachelorThesis>> createObjekts(CSVRecord csvRecord,
+			List<BachelorThesis> bachelorThesis, List<Reviewer> listreviewer, List<Author> listauthor) {
 		String name = csvRecord.get("Name, Vorname");
-		String studyGroup = csvRecord.get("Studien-\n" + 
-				"gruppe");
+		String studyGroup = csvRecord.get("Studien-\n" + "gruppe");
 		String company = csvRecord.get("Praxispartner");
 		String topic = csvRecord.get("Themenvorschlag Bachelor Thesis");
-		String firstreviewer = csvRecord.get("Dozent\n" + 
-				"1. Gutachten");
-		String secondreviewer = csvRecord.get("Dozent\n" + 
-				"2. Gutachten");
+		String firstreviewer = csvRecord.get("Dozent\n" + "1. Gutachten");
+		String secondreviewer = csvRecord.get("Dozent\n" + "2. Gutachten");
 		String commant = csvRecord.get("Bemerkung");
-		
+
 // wenn die Header nicht so  wichtig sind und wir davon ausgehen, das die Datei im richtigen Format ist
 //		String name = csvRecord.get(0);
 //		String studyGroup = csvRecord.get(1);
@@ -49,7 +48,7 @@ public class CSVMapper {
 //		String firstreviewer = csvRecord.get(4);
 //		String secondreviewer = csvRecord.get(5);
 //		String commant = csvRecord.get(6);
-		
+
 		System.out.println(name + firstreviewer + secondreviewer);
 		if (name == null || name.isBlank()) {
 			return Pair.of(listreviewer, bachelorThesis);
@@ -95,6 +94,32 @@ public class CSVMapper {
 		Reviewer reviewer = new Reviewer(name, 10);
 		listreviewer.add(reviewer);
 		return reviewer;
+	}
+
+	public static void maptoCSV(CSVPrinter csvPrinter, List<BachelorThesis> bachelorTheses) {
+
+		for (BachelorThesis bachelorThesis : bachelorTheses) {
+
+			createObjekts(csvPrinter, bachelorThesis);
+		}
+
+	}
+
+	private static void createObjekts(CSVPrinter csvPrinter, BachelorThesis bachelorThesis) {
+		String topic = bachelorThesis.getTopic();
+		String commant = bachelorThesis.getCommant();
+		String name = bachelorThesis.getAuthor().getName();
+		String studyGroup = bachelorThesis.getAuthor().getStudyGroup();
+		String company = bachelorThesis.getAuthor().getCompany();
+		String firstreviewer = bachelorThesis.getFirstReview().getReviewer().getName();
+		String secondreviewer = bachelorThesis.getSecondReview().get().getReviewer().getName();
+
+		try {
+			csvPrinter.printRecord(name, studyGroup, company, topic, firstreviewer, secondreviewer, commant);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
