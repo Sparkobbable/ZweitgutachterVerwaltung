c31fec82a5f0680f9c920e3e4ef51aaf0a03b568
Jakob <j.wilmesmeier@gmail.com>
2020-12-10 19:49:22 +0100

    Kodierung weiterer UTF-8 Zeichen

--- src/util/UmlautFormatter.java
+++ src/util/UmlautFormatter.java
@@ -8,7 +8,8 @@ public class UmlautFormatter {
 	private static final Pair<String, String> UE = new Pair<String, String>("Ã¼", "ü");
 	private static final Pair<String, String> AE = new Pair<String, String>("Ã¤", "ä");
 	private static final Pair<String, String> OE = new Pair<String, String>("Ã¶", "ö");
-	private static final Pair<List<String>, String> SZ = new Pair<List<String>, String>(List.of("ÃŸ", "Ã"), "ß");
+	private static final Pair<List<String>, String> SZ = new Pair<List<String>, String>(List.of("ÃŸ", "Ã"), "ß"); //Second UTF-8 Code for ß is not visible but there
+	private static final Pair<String, String> EURO = new Pair<String, String>("", "€"); //UTF-8 Code for € is not visible but there
 	
 	public static String format(String stringToFormat) {
 		if (stringToFormat.contains(UE.getLeft())) {
@@ -19,6 +20,9 @@ public class UmlautFormatter {
 		}
 		if (stringToFormat.contains(OE.getLeft())) {
 			stringToFormat = stringToFormat.replace(OE.getLeft(), OE.getRight());
+		}
+		if (stringToFormat.contains(EURO.getLeft())) {
+			stringToFormat = stringToFormat.replace(EURO.getLeft(), EURO.getRight());
 		}
 		if (stringToFormat.contains(SZ.getLeft().get(0)) || stringToFormat.contains(SZ.getLeft().get(1))) {
 			stringToFormat = stringToFormat.replace(SZ.getLeft().get(0), SZ.getRight());
