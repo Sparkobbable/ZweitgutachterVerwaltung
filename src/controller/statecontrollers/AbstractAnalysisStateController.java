a2350af40559fc85a84b2714e12242a6973e6b24
CapOfCave <lars.kecker2009@gmail.com>
2020-12-18 16:50:07 +0100

    Javadoc

--- src/controller/statecontrollers/AbstractAnalysisStateController.java
+++ src/controller/statecontrollers/AbstractAnalysisStateController.java
@@ -27,34 +27,35 @@ import view.widgets.PieChart;
 
 /**
  * Abstract ApplicationStateController which includes all methods that are
- * needed for every AnalysisStateController. They are divided because they
- * apply to different ApplicationStates. Although they have methods in common.
- * They inherit them from this class.
+ * needed for every AnalysisStateController. They are divided because they apply
+ * to different ApplicationStates. Although they have methods in common. They
+ * inherit them from this class.
  */
 public abstract class AbstractAnalysisStateController extends AbstractStateController {
 
 	protected ComboBoxMode reviewerFilter;
-	
-	public AbstractAnalysisStateController(ApplicationState applicationState,
-			View view, Controller controller, Model model) {
+
+	public AbstractAnalysisStateController(ApplicationState applicationState, View view, Controller controller,
+			Model model) {
 		super(applicationState, view, controller, model);
 		this.reviewerFilter = ComboBoxMode.FIRSTREVIEWER;
 		this.registerEvent(EventId.INITIALIZE, (params) -> this.initialize());
 	}
-	
+
 	private void initialize() {
 		this.switchData(this.reviewerFilter);
 	}
-	
+
 	/**
-	 * Switches between different graphics including {@link AnalysisReviewerTableModel}, 
-	 * {@link PieChart} and {@link BarChart}within the {@link AnalysisPanel}.
+	 * Switches between different graphics including
+	 * {@link AnalysisReviewerTableModel}, {@link PieChart} and
+	 * {@link BarChart}within the {@link AnalysisPanel}.
 	 * 
 	 * @param params - Selected presentationMode from comboBox in
-	 *               {@link AnalysisOptionsPanel}
+	 *               AnalysisOptionsPanel
 	 */
 	protected void switchPresentation(ComboBoxMode params) {
-		switch(params) {
+		switch (params) {
 		case TABLE:
 			this.switchState(ApplicationState.ANALYSE_TABLE);
 			break;
@@ -71,12 +72,13 @@ public abstract class AbstractAnalysisStateController extends AbstractStateContr
 			throw new IllegalArgumentException("Invalid PresentationMode from ComboBox");
 		}
 	}
-	
+
 	/**
-	 * Switches between different data in percentage which
-	 * will get presented in the {@link PieChart}, {@link BarChart} or {@link ReviewerOverviewTableModel}.
+	 * Switches between different data in percentage which will get presented in the
+	 * {@link PieChart}, {@link BarChart} or {@link ReviewerOverviewTableModel}.
 	 * 
-	 * @param reviewerFilter - selected dataMode from {@link ComboBoxPanel} in {@link AnalysisOptionsPanelOptionsPanel}
+	 * @param reviewerFilter - selected dataMode from {@link ComboBoxPanel} in
+	 *                       {@link AnalysisOptionsPanelOptionsPanel}
 	 */
 	protected void switchData(ComboBoxMode reviewerFilter) {
 		this.reviewerFilter = reviewerFilter;
@@ -105,27 +107,28 @@ public abstract class AbstractAnalysisStateController extends AbstractStateContr
 	 */
 	protected ArrayList<Reviewer> getAllFirstReviewers() {
 		ArrayList<Reviewer> result = new ArrayList<>();
-		for(Reviewer currentReviewer : this.model.getReviewers()) {
-			for(Review currentReview : currentReviewer.getFirstReviews()) {
-				if(!result.contains(currentReviewer)) {
+		for (Reviewer currentReviewer : this.model.getReviewers()) {
+			for (Review currentReview : currentReviewer.getFirstReviews()) {
+				if (!result.contains(currentReviewer)) {
 					result.add(currentReview.getReviewer());
 				}
 			}
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Get all Reviewer, which supervise at least one {@link SecondReview}
 	 * 
 	 * @return ArrayList<Reviewer>
 	 */
 	protected ArrayList<Reviewer> getAllSecondReviewers() {
-		ArrayList<Reviewer> result1 = new ArrayList<>();;
-		for(Reviewer currentReviewer : this.model.getReviewers()) {
-			for(SecondReview currentReview : currentReviewer.getUnrejectedSecondReviews()) {
-				if(currentReview.getStatus() == ReviewStatus.APPROVED) {
-					if(!result1.contains(currentReviewer)) {
+		ArrayList<Reviewer> result1 = new ArrayList<>();
+		;
+		for (Reviewer currentReviewer : this.model.getReviewers()) {
+			for (SecondReview currentReview : currentReviewer.getUnrejectedSecondReviews()) {
+				if (currentReview.getStatus() == ReviewStatus.APPROVED) {
+					if (!result1.contains(currentReviewer)) {
 						result1.add(currentReview.getReviewer());
 					}
 				}
@@ -133,7 +136,7 @@ public abstract class AbstractAnalysisStateController extends AbstractStateContr
 		}
 		return result1;
 	}
-	
+
 	/**
 	 * Get all Reviewer, which supervise at least one {@link Review}
 	 * 
@@ -145,28 +148,31 @@ public abstract class AbstractAnalysisStateController extends AbstractStateContr
 		result.addAll(this.getAllSecondReviewers());
 		return result;
 	}
-	
+
 	/**
-	 * Count the amount of supervised {@link FirstReview} & {@link SecondReview} and create the 
-	 * corresponding data structure
+	 * Count the amount of supervised {@link FirstReview} & {@link SecondReview} and
+	 * create the corresponding data structure
 	 * 
 	 * @param List<Reviewer reviewers
 	 * @return Map<Reviewer, Pair<Optional<Integer>, Optional<Integer>>> ReviewCount
 	 */
 	protected Map<Reviewer, Pair<Optional<Integer>, Optional<Integer>>> getReviewCount(List<Reviewer> reviewers) {
 		HashMap<Reviewer, Pair<Optional<Integer>, Optional<Integer>>> result = new HashMap<>();
-		switch(this.reviewerFilter) {
+		switch (this.reviewerFilter) {
 		case FIRSTREVIEWER:
-			reviewers.forEach(reviewer -> result.put(reviewer, 
-					(new Pair<Optional<Integer>, Optional<Integer>>(Optional.of(reviewer.getFirstReviewCount()), Optional.empty()))));
+			reviewers.forEach(reviewer -> result.put(reviewer,
+					(new Pair<Optional<Integer>, Optional<Integer>>(Optional.of(reviewer.getFirstReviewCount()),
+							Optional.empty()))));
 			break;
 		case SECONDREVIEWER:
-			reviewers.forEach(reviewer -> result.put(reviewer, 
-					new Pair<Optional<Integer>, Optional<Integer>>(Optional.empty(), Optional.of(reviewer.getApprovedSecondReviewCount()))));
+			reviewers.forEach(
+					reviewer -> result.put(reviewer, new Pair<Optional<Integer>, Optional<Integer>>(Optional.empty(),
+							Optional.of(reviewer.getApprovedSecondReviewCount()))));
 			break;
 		case ALLREVIEWER:
-			reviewers.forEach(reviewer -> result.put(reviewer, 
-					new Pair<Optional<Integer>, Optional<Integer>>(Optional.of(reviewer.getFirstReviewCount()), Optional.of(reviewer.getApprovedSecondReviewCount()))));
+			reviewers.forEach(reviewer -> result.put(reviewer,
+					new Pair<Optional<Integer>, Optional<Integer>>(Optional.of(reviewer.getFirstReviewCount()),
+							Optional.of(reviewer.getApprovedSecondReviewCount()))));
 			break;
 		default:
 			throw new IllegalArgumentException("Invalid DataMode for currentDataStatus");
